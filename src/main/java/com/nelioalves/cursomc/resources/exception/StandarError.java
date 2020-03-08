244dae1c32fbc5b6026440b569652e2943f0e4ac
franciole <franciole.bezerra@gmail.com>
2020-03-08 17:06:57 -0300

    Padronizando formato de Excecoes

--- src/main/java/com/nelioalves/cursomc/resources/exception/StandarError.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/StandarError.java
@@ -6,43 +6,63 @@ public class StandarError implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private Integer id;
-	private String msg;
-	private Long timeStamp;
+	private Long timestamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
 
-	public StandarError() {
-
-	}
-
-	public StandarError(Integer id, String msg, Long timeStamp) {
+	public StandarError(Long timestamp, Integer status, String error, String message, String path) {
 		super();
-		this.id = id;
-		this.msg = msg;
-		this.timeStamp = timeStamp;
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
 	}
 
-	public Integer getId() {
-		return id;
+	public Long getTimestamp() {
+		return timestamp;
 	}
 
-	public void setId(Integer id) {
-		this.id = id;
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
 	}
 
-	public String getMsg() {
-		return msg;
+	public Integer getStatus() {
+		return status;
 	}
 
-	public void setMsg(String msg) {
-		this.msg = msg;
+	public void setStatus(Integer status) {
+		this.status = status;
 	}
 
-	public Long getTimeStamp() {
-		return timeStamp;
+	public String getError() {
+		return error;
 	}
 
-	public void setTimeStamp(Long timeStamp) {
-		this.timeStamp = timeStamp;
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
 
 }
